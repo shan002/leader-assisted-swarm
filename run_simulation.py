45b0837aaaac2fa41102faf0039a3eca4a8aff63
shan002 <shantanushan002@gmail.com>
2026-07-13 03:49:12 -0400

    removed unrelated controller registrations from run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -7,9 +7,6 @@ from GoalSeekingController import GoalSeekingController
 from BinaryPriorityController import BinaryPriorityController
 
 
-register_dictlike_type("controller", "RCController", RCController)
-register_dictlike_type("controller", "GoalSeekingController", GoalSeekingController)
-register_dictlike_type("controller", "BinaryPriorityController", BinaryPriorityController)
 world_config = RectangularWorldConfig.from_yaml("world.yaml")
 world = RectangularWorld(world_config)
 sim(world)
